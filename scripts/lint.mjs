3fedc98e44409f7416c49c82a10fd1dbc391ee20
jedrichards <jedrichards@gmail.com>
2019-09-13 14:22:27 +0100

    Add symbol schemas

--- scripts/lint.mjs
+++ scripts/lint.mjs
@@ -4,6 +4,8 @@
  * - `{ $ref: ... }` schemas should contain no other keywords
  * - All `$ref` values should map to a real file on the file system
  * - Whitelist supported keywords
- * - Every schema file should have a populated title and description
+ * - Every schema file should have a populated `title` and `description`
  * - Check that properties referenced in `required` are present in the schema
+ * - Do we want to enforce `description` on _all_ properties?
+ * - ...
  */
