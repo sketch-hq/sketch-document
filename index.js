2ec02dd4963090dfca7fe968bdebb15b82ac9072
jedrichards <jedrichards@gmail.com>
2019-10-31 13:15:50 +0000

    feat: support for Sketch document version 120
    Support for smart layout settings and numerous other schema changes around objects with default
    values now being persisted to JSON rather than being omitted
    
    BREAKING CHANGE: The schemas will no longer validate Sketch files for document versions less than
    120

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.version = 119
+exports.version = 120
 exports.document = require('./dist/document.schema.json')
 exports.fileFormat = require('./dist/file-format.schema.json')
 exports.meta = require('./dist/meta.schema.json')
