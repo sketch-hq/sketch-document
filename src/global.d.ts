aedf99318e455cbdb63841ad1d79dcc712fcfe82
Jed Richards <jedrichards@gmail.com>
2020-02-18 16:42:51 +0000

    feat: add validate-file script (#62)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,22 @@
 declare module 'deepdash'
 
 declare module 'json-transforms'
+
+declare module 'node-stream-zip' {
+  export default class StreamZip {
+    public constructor(options: { file: string; storeEntries: boolean })
+
+    public on(event: 'ready', handler: () => void): void
+
+    public on(event: 'error', handler: (error: string) => void): void
+
+    public entryDataSync(name: string): string
+
+    public stream(
+      ref: string,
+      handler: (error: string, stream: NodeJS.ReadStream) => void,
+    ): void
+
+    public close(): void
+  }
+}
---
+++ src/validate-file.ts
@@ -0,0 +1,64 @@
+import StreamZip from 'node-stream-zip'
+import type { ErrorObject } from 'ajv'
+import util from 'util'
+
+import { getAjvInstance } from './utils'
+import schema from './file-format.schema.json'
+
+// TODO: This `fromFile` (and presumably a `toFile`) function should be
+// published publically in the future
+const fromFile = async (filepath: string): Promise<any> => {
+  const archive = new StreamZip({
+    file: filepath,
+    storeEntries: true,
+  })
+
+  const contents = await new Promise((resolve): void => {
+    archive.on('ready', (): void => {
+      const document = JSON.parse(
+        archive.entryDataSync('document.json').toString(),
+      )
+      const pages = document.pages.map((page: { _ref: string }): void =>
+        JSON.parse(archive.entryDataSync(`${page._ref}.json`).toString()),
+      )
+      resolve({
+        document: {
+          ...document,
+          pages,
+        },
+        meta: JSON.parse(archive.entryDataSync('meta.json').toString()),
+        user: JSON.parse(archive.entryDataSync('user.json').toString()),
+      })
+    })
+  })
+
+  archive.close()
+
+  return contents
+}
+
+const main = async () => {
+  const filepath = process.argv[2]
+  const file = await fromFile(filepath)
+  const ajv = getAjvInstance()
+  ajv.validate(schema, file)
+  if (ajv.errors) {
+    let output = ''
+    ajv.errors!.forEach((error: ErrorObject) => {
+      const { keyword, dataPath, schemaPath, params, message, data } = error
+      output += '\n'
+      output += `message: ${message}\n`
+      output += `keyword: ${keyword}\n`
+      output += `schemaPath: ${schemaPath}\n`
+      output += `dataPath: ${dataPath}\n`
+      output += `params: ${util.inspect(params, { depth: 0 })}\n`
+      output += `data: ${util.inspect(data, { depth: 0 })}\n`
+    })
+    console.log(output)
+    process.exit(1)
+  } else {
+    console.log(`File valid`)
+  }
+}
+
+main()
