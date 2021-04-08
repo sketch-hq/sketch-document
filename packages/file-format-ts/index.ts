2660bc4a6b48229d633bc9de7d6ea54898a60d3e
Christian Klotz <hello@christianklotz.co.uk>
2021-04-08 14:42:34 +0100

    Use only latest schemas

--- packages/file-format-ts/index.ts
+++ packages/file-format-ts/index.ts
@@ -1,7 +1,3 @@
-import * as FileFormat1 from './v1-types.js'
-import * as FileFormat2 from './v2-types.js'
-import * as FileFormat3 from './v3-types.js'
+import * as FileFormat from './types.js'
 
-export { FileFormat1, FileFormat2, FileFormat3 }
-
-export default FileFormat3
+export default FileFormat
