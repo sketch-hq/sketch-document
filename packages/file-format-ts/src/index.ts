f7355f6165515d58d7aa93066c59c2f5bc50fc40
Christian Klotz <hello@christianklotz.co.uk>
2021-04-13 10:37:22 +0100

    Update comment

--- packages/file-format-ts/src/index.ts
+++ packages/file-format-ts/src/index.ts
@@ -1,5 +1,13 @@
-// Types must be generated using using `yarn generate` before they can
-// be imported.
+/**
+ * Types must be generated from the {@link ../../file-format|JSON Schema definitions}.
+ *
+ * To always use the latest schemas, either run `yarn build` from the monorepo root
+ * which first builds {@link @sketch-hq/sketch-file-format} before generating the
+ * types.
+ *
+ * Alternatively, running `yarn generate` from {@link ../|file-format-ts}  generates
+ * the TypeScript types only, based on the previous build of the schemas.
+ */
 import * as FileFormat from './types'
 
 export default FileFormat
